66bfaff69c89809c4f825b96452c63acef0e76cd
Youssef Salah <joe.is.writing.code@gmail.com>
2025-03-22 16:26:04 +0300

    File is Deleted

--- Bank_System.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-
-    std::cout << "hello Joe\n";
-    std::cout << "Ya Allah\n";
-    std::cout << "I don't understand anything\n";
-    std::cout << "hello github\n";
-}
